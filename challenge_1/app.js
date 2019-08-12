8636234eb2abbaf6b73ae9b4c5a46117c75b1467
Preston Whitehall <pmwhitehall@gmail.com>
2019-08-12 14:30:48 -0700

    Added a board array and value toggling in prep for scoring

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -1,3 +1,9 @@
+const gameBoard = [
+  [0, 0, 0],
+  [0, 0, 0],
+  [0, 0, 0]
+];
+
 const togglePiece = () => {
   let player = 'O';
   return () => {
@@ -12,3 +18,14 @@ const togglePiece = () => {
 
 // Toggles a location between 'X' and 'O'
 const toggle = togglePiece();
+
+// Updates board placement for calculating wins
+const updateGameBoard = (coord, player) => {
+  let value = 0;
+  if (player === 'O') {
+    value = 1;
+  } else {
+    value = 2;
+  }
+  gameBoard[coord[0]][coord[1]] = value;
+}
--- challenge_1/listeners.js
+++ challenge_1/listeners.js
@@ -3,6 +3,8 @@
 document.addEventListener('click', (event) => {
   if (event.target.matches('.box')) {
     toggle.call(event);
+    let coord = JSON.parse(event.target.id);
+    updateGameBoard(coord, event.target.innerText);
   }
 
 });
