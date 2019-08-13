1b9971c474fb6ae1200254dfb54e6964fa320aa9
Preston Whitehall <pmwhitehall@gmail.com>
2019-08-13 11:39:42 -0700

    Initial server setup: 'Hello World'

--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,2 +1,12 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+const morgan = require('morgan');
 
+app.use(morgan());  
 
+app.get('/', (req, res) => {
+  res.send('Hello World');
+});
+
+app.listen(port, () => {});
