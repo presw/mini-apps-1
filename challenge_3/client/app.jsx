4fc191e1d7ab15881f003ab615e654d0ae10b5b4
Preston Whitehall <pmwhitehall@gmail.com>
2019-08-14 16:02:30 -0700

    Hello World react page loading

--- challenge_3/client/app.jsx
+++ challenge_3/client/app.jsx
@@ -1,2 +1,21 @@
 
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      // stuff here
+    }
+  }
 
+  componentDidMount() {
+    // do something
+  }
+
+  render() {
+    return (
+      <h1>Hello world</h1>
+    )
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById('app'));
--- challenge_3/server.js
+++ challenge_3/server.js
@@ -1,2 +1,15 @@
+const express = require('express');
+const morgan = require('morgan');
+const app = express();
+const port = 3000;
+const path = require('path');
 
+app.use('/public', express.static('./public/'));
 
+app.use(morgan('combined'));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '/public/index.html'));
+});
+
+app.listen(port, () => console.log(`Listening on port ${port}!`))
