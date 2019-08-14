321e89b689589eb78d1575ee2c253cd78cd25a98
Preston Whitehall <pmwhitehall@gmail.com>
2019-08-13 21:32:45 -0700

    Changed router back to app variable for clarity

--- challenge_2/server/server.js
+++ challenge_2/server/server.js
@@ -1,23 +1,23 @@
 const express = require('express');
-const router = express();
+const app = express();
 const port = 3000;
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const functions = require('./functions');
 
-router.use(express.static('../client'));
-router.use(morgan('combined'));
+app.use(express.static('../client'));
+app.use(morgan('combined'));
 
-router.use(bodyParser.json());
-router.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
-router.get('/', (req, res) => {
+app.get('/', (req, res) => {
   res.sendFile('index');
 });
 
-router.post('/', (req, res) => {
+app.post('/', (req, res) => {
   let outputString = functions.jsonToCsv(req.body.text);
   res.end();
 });
 
-router.listen(port, () => { console.log(`Listening on port: ${port}`) });
+app.listen(port, () => { console.log(`Listening on port: ${port}`) });
