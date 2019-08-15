42b5b00026dfef9f0b142668a5f98b12883562c7
Preston Whitehall <pmwhitehall@gmail.com>
2019-08-14 21:09:17 -0700

    Added ability to store entered data in state; clears on complete

--- challenge_3/client/app.jsx
+++ challenge_3/client/app.jsx
@@ -4,31 +4,29 @@ class App extends React.Component {
     super(props)
     this.state = {
       currentView: FrontPage,
-      userData: {
-        name: null,
-        email: null,
-        password: null,
-      },
-      locData: {
-        address1: null,
-        address2: null,
-        city: null,
-        state: null,
-        zipCode: null,
-        phoneNum: null,
-      },
-      cardData: {
-        cardNum: null,
-        expires: null,
-        cvv: null,
-        billingCode: null,
-      },
+      // User data
+      name: null,
+      email: null,
+      password: null,
+      // Location data
+      address1: null,
+      address2: null,
+      city: null,
+      state: null,
+      zipCode: null,
+      phoneNum: null,
+      // Card data
+      cardNum: null,
+      expires: null,
+      cvv: null,
+      billingCode: null,
     }
     this.clickCheckout = this.clickCheckout.bind(this);
     this.submitF1 = this.submitF1.bind(this);
     this.submitF2 = this.submitF2.bind(this);
     this.submitF3 = this.submitF3.bind(this);
     this.returnToMain = this.returnToMain.bind(this);
+    this.onFieldChange = this.onFieldChange.bind(this);
   }
 
   componentDidMount() {
@@ -52,7 +50,27 @@ class App extends React.Component {
   }
 
   returnToMain() {
-    this.setState({ currentView: FrontPage });
+    this.setState({
+      currentView: FrontPage,
+      name: null,
+      email: null,
+      password: null,
+      address1: null,
+      address2: null,
+      city: null,
+      state: null,
+      zipCode: null,
+      phoneNum: null,
+      cardNum: null,
+      expires: null,
+      cvv: null,
+      billingCode: null,
+    });
+  }
+
+  onFieldChange(event) {
+    let key = event.target.id;
+    this.setState({ [key]: event.target.value });
   }
 
   render() {
@@ -63,15 +81,15 @@ class App extends React.Component {
         <this.state.currentView checkout={this.clickCheckout} />
       ) :
       this.state.currentView === F1 ? (
-        <this.state.currentView submitF1={this.submitF1} />
+        <this.state.currentView onFieldChange={this.onFieldChange} submitF1={this.submitF1} />
       ) :
       this.state.currentView === F2 ? (
-        <this.state.currentView submitF2={this.submitF2} />
+        <this.state.currentView onFieldChange={this.onFieldChange} submitF2={this.submitF2} />
       ) :
       this.state.currentView === F3 ? (
-        <this.state.currentView submitF3={this.submitF3} />
+        <this.state.currentView onFieldChange={this.onFieldChange} submitF3={this.submitF3} />
       ) : (
-        <this.state.currentView returnToMain={this.returnToMain} props={this.state}/>
+        <this.state.currentView onFieldChange={this.onFieldChange} returnToMain={this.returnToMain} props={this.state}/>
       )
       }
     </div>
@@ -91,9 +109,9 @@ const F1 = (props) => {
   return (
     <div className="input-fields">
       <div>Please Login</div>
-      Name: <input placeholder="Name" type="text"></input><br></br>
-      Email: <input placeholder="Email" type="text"></input><br></br>
-      Password: <input placeholder="Password" type="text"></input><br></br>
+      Name: <input onChange={props.onFieldChange} id="name" placeholder="Name" type="text"></input><br></br>
+      Email: <input onChange={props.onFieldChange} id="email" placeholder="Email" type="text"></input><br></br>
+      Password: <input onChange={props.onFieldChange} id="password" placeholder="Password" type="text"></input><br></br>
       <button onClick={props.submitF1}>Submit</button>
     </div>
   )
@@ -103,12 +121,12 @@ const F2 = (props) => {
   return (
     <div className="input-fields">
       <div>Please delivery address</div>
-      Address: <input placeholder="Address" type="text"></input><br></br>
-      Apt: <input placeholder="Apt" type="text"></input><br></br>
-      City: <input placeholder="City" type="text"></input><br></br>
-      State: <input placeholder="State" type="text"></input><br></br>
-      Zip Code: <input placeholder="Zip Code" type="text"></input><br></br>
-      Phone Number: <input placeholder="Phone Number" type="text"></input><br></br>
+      Address: <input onChange={props.onFieldChange} id="address1" placeholder="Address" type="text"></input><br></br>
+      Apt: <input onChange={props.onFieldChange} id="address2" placeholder="Apt" type="text"></input><br></br>
+      City: <input onChange={props.onFieldChange} id="city" placeholder="City" type="text"></input><br></br>
+      State: <input onChange={props.onFieldChange} id="state" placeholder="State" type="text"></input><br></br>
+      Zip Code: <input onChange={props.onFieldChange} id="zipCode" placeholder="Zip Code" type="text"></input><br></br>
+      Phone Number: <input onChange={props.onFieldChange} id="phoneNum" placeholder="Phone Number" type="text"></input><br></br>
       <button onClick={props.submitF2}>Submit</button>
     </div>
   )
@@ -120,10 +138,10 @@ const F3 = (props) => {
   return (
     <div className="input-fields">
       <div>Please payment details</div>
-      Credit Card: <input placeholder="Credit Card" type="text"></input><br></br>
-      Expiration: <input placeholder="Expiration" type="text"></input><br></br>
-      CVV: <input placeholder="CVV" type="text"></input><br></br>
-      Billing Zip Code: <input placeholder="Billing Zip Code" type="text"></input><br></br>
+      Credit Card: <input onChange={props.onFieldChange} id="cardNum" placeholder="Credit Card" type="text"></input><br></br>
+      Expiration: <input onChange={props.onFieldChange} id="expires" placeholder="Expiration" type="text"></input><br></br>
+      CVV: <input onChange={props.onFieldChange} id="cvv" placeholder="CVV" type="text"></input><br></br>
+      Billing Zip Code: <input onChange={props.onFieldChange} id="billingCode" placeholder="Billing Zip Code" type="text"></input><br></br>
       <button onClick={props.submitF3}>Submit</button>
     </div>
   )
@@ -134,20 +152,20 @@ const Confirm = (props) => {
   return (
     <div className="confirmation-page">
       Confirmation page lives here.
-      <p><b>Name: </b>{props.props.userData.name}</p>
-      <p><b>Email: </b>{props.props.userData.email}</p>
-      <p><b>Password: </b>{props.props.userData.password}</p>
-      <p><b>Address1: </b>{props.props.locData.address1}</p>
-      <p><b>Address2: </b>{props.props.locData.address2}</p>
-      <p><b>City: </b>{props.props.locData.city}</p>
-      <p><b>State: </b>{props.props.locData.state}</p>
-      <p><b>Zip Code: </b>{props.props.locData.zipCode}</p>
-      <p><b>Phone Number: </b>{props.props.locData.phoneNum}</p>
-      <p><b>Card #: </b>{props.props.cardData.cardNum}</p>
-      <p><b>Expires: </b>{props.props.cardData.expires}</p>
-      <p><b>CVV: </b>{props.props.cardData.cvv}</p>
-      <p><b>Billing Zip Code: </b>{props.props.userData.billingCode}</p>
-      <button onClick={props.returnToMain}>Return to main page</button>
+      <p><b>Name: </b>{props.props.name}</p>
+      <p><b>Email: </b>{props.props.email}</p>
+      <p><b>Password: </b>{props.props.password}</p>
+      <p><b>Address1: </b>{props.props.address1}</p>
+      <p><b>Address2: </b>{props.props.address2}</p>
+      <p><b>City: </b>{props.props.city}</p>
+      <p><b>State: </b>{props.props.state}</p>
+      <p><b>Zip Code: </b>{props.props.zipCode}</p>
+      <p><b>Phone Number: </b>{props.props.phoneNum}</p>
+      <p><b>Card #: </b>{props.props.cardNum}</p>
+      <p><b>Expires: </b>{props.props.expires}</p>
+      <p><b>CVV: </b>{props.props.cvv}</p>
+      <p><b>Billing Zip Code: </b>{props.props.billingCode}</p>
+      <button onClick={props.returnToMain}>Purchase</button>
     </div>
   )
 }
