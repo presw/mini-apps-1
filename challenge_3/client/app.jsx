0fd96b52b9636d22b7e30c59d843622ee7a5d9f5
Preston Whitehall <pmwhitehall@gmail.com>
2019-08-14 17:25:30 -0700

    Added data holders to state; added method for changing between views

--- challenge_3/client/app.jsx
+++ challenge_3/client/app.jsx
@@ -3,19 +3,89 @@ class App extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      // stuff here
+      currentView: FrontPage,
+      userData: {
+        name: null,
+        email: null,
+        password: null,
+      },
+      locData: {
+        address1: null,
+        address2: null,
+        city: null,
+        state: null,
+        zipCode: null,
+      },
+      cardData: {
+        cardNum: null,
+        expires: null,
+        cvv: null,
+        zipCode: null,
+      },
     }
+    this.clickCheckout = this.clickCheckout.bind(this);
   }
 
   componentDidMount() {
     // do something
   }
 
+  clickCheckout() {
+    this.setState({ currentView: F1 });
+  }
+
+  submitF1() {
+    this.setState({ currentView: F2 });
+  }
+
   render() {
     return (
-      <h1>Hello world</h1>
+      <div>
+      <h1>Nozama - Buy Stuff!</h1>
+      {this.state.currentView === FrontPage ? (
+        <this.state.currentView checkout={this.clickCheckout} />
+      ) :
+      this.state.currentView === F1 ? (
+        <this.state.currentView />
+      ) :
+      this.state.currentView === F2 ? (
+        <this.state.currentView /> // F2 presumably
+      ) :
+      this.state.currentView === F3 ? (
+        <this.state.currentView /> // F3 presumably
+      ) : (
+        <this.state.currentView /> // Confirm presumably
+      )
+      }
+    </div>
     )
   }
+}
+
+const FrontPage = (props) => {
+  return (
+    <div>
+      <button onClick={props.checkout} >Checkout</button>
+    </div>
+  )
+}
+
+// class F1 extends React.Component {
+//   constructor (props) {
+//     super(props);
+//     this.state = {
+//     }
+//   }
+// }
+const F1 = (props) => {
+  return (
+    <div className="input-fields">
+      <div>Please Login</div>
+      Name: <input type="text"></input><br></br>
+      Email: <input type="text"></input><br></br>
+      Password: <input type="text"></input><br></br>
+    </div>
+  )
 }
 
 ReactDOM.render(<App />, document.getElementById('app'));
