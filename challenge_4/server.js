fa46e9fa391b119679f24eeafe4281ea6f66512f
Preston Whitehall <pmwhitehall@gmail.com>
2019-08-15 12:33:11 -0700

    Initial setup of dependencies & 'hello world' site

--- challenge_4/server.js
+++ challenge_4/server.js
@@ -1,2 +1,17 @@
+const express = require('express');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const app = express();
+const port = 4321;
 
+app.use(morgan('combined'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static('public'));
 
+app.get('/', (req, res) => {
+  res.sendFile('/public/index');
+  res.end(); // NOTE: unnecessary?
+})
+
+app.listen(port, () => { console.log(`Listening on port: ${port}`); });
