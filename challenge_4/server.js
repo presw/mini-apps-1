93843fddf520905c15322786b117b4073d4c2e82
Preston Whitehall <pmwhitehall@gmail.com>
2019-08-15 17:53:33 -0700

    React page rendering

--- challenge_4/server.js
+++ challenge_4/server.js
@@ -1,17 +1,17 @@
 const express = require('express');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
+const path = require('path');
 const app = express();
 const port = 4321;
 
 app.use(morgan('combined'));
 app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, '/public/dist/')));
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-  res.sendFile('/public/index');
-  res.end(); // NOTE: unnecessary?
+  res.end();
 })
 
 app.listen(port, () => { console.log(`Listening on port: ${port}`); });
